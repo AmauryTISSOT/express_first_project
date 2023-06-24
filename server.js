46d425173219ec35b0fa59fca2b1c74f2eb0f070
amaury <tissot.amaury@gmail.com>
2023-06-24 22:23:51 +0200

    Add router

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express();
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index" , {name123 : "paul"})
 })
+
+const userRouter = require("./routes/users")
+
+app.use("/users", userRouter)
 
 app.listen(3000)
